22cc8e36bf470beb78064f9fd97383bead257ad1
Mihai Babet <mihai.babet@gmail.com>
2025-03-04 00:14:36 +0200

    cleanup

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,5 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
-  networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-  },
+  solidity: "0.8.28"
 };

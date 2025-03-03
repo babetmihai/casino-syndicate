5bd88c185926d49385dadf1ee7e500ad56b2717b
Mihai Babet <mihai.babet@gmail.com>
2025-03-04 00:42:04 +0200

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+// or require("@nomiclabs/hardhat-waffle"); for older versions
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
   networks: {
     hardhat: {
-      chainId: 1337,
+      chainId: 1337 // Standard local chain ID
     },
-  },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337
+    }
+  }
 };

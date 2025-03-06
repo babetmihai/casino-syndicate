f7e1aac5a6cd45b8f9a69ea7951c575189c83454
Mihai Babet <mihai.babet@gmail.com>
2025-03-06 23:58:43 +0200

    init contract

--- app/core/contract.js
+++ app/core/contract.js
@@ -1,16 +1,13 @@
 import { ethers } from "ethers"
-import { getWalletProvider } from "./wallet"
 import { actions } from "./store"
 
 
 const { VITE_CONTRACT_NAME } = import.meta.env
 
-export const initContract = async () => {
+export const initContract = async (account) => {
   const config = await fetch(`/${VITE_CONTRACT_NAME}.json?v=${Date.now()}`)
     .then(res => res.json())
-  const provider = await getWalletProvider()
-  const signer = await provider.getSigner()
-  const contract = new ethers.Contract(config.address, config.abi, signer)
+  const contract = new ethers.Contract(config.address, config.abi, account)
   actions.set("wallet.contract", contract)
 }
 

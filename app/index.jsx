55c3e259f13c12c312ff13ca845d3d920b4cbe15
Mihai Babet <mihai.babet@gmail.com>
2025-03-06 18:55:48 +0200

    cleanup

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,9 @@
+import "@mantine/core/styles.css"
+import "./index.css"
+
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-
-import "@mantine/core/styles.css"
-import "./index.css"
 import { Provider } from "react-redux"
 import store from "./core/store"
 import { loadStorage } from "./core/storage"

82710ee606a773f84508ebc4e84372247e777b28
Mihai Babet <mihai.babet@gmail.com>
2025-03-06 18:51:18 +0200

    cleanup

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: './app/public',
+  publicDir: "./app/public",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './app')
+      "@": path.resolve(__dirname, "./app")
     }
   }
 })

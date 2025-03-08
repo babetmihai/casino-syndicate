944ce42e656893df1407604ff43933b61df38f4c
Mihai Babet <mihai.babet@gmail.com>
2025-03-08 22:49:47 +0200

    fix balance

--- app/screens/Dashboard/index.jsx
+++ app/screens/Dashboard/index.jsx
@@ -2,9 +2,28 @@ import React from "react"
 import AppScreen from "app/components/AppScreen"
 import { Button } from "@mantine/core"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectWallet } from "app/core/wallet"
 
 
 const DashboardScreen = () => {
+  const { contract, account } = useSelector(() => selectWallet())
+
+  React.useEffect(() => {
+    if (contract && account) {
+      const init = async () => {
+        console.log("init")
+        try {
+          const balance = await contract.getContractBalance()
+          console.log("Balance:", balance)
+        } catch (error) {
+          console.error("Failed to fetch balance:", error)
+        }
+      }
+      init()
+    }
+
+  }, [contract, account])
   return (
     <AppScreen name="Dashboard">
       <div>

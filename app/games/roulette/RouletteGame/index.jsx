2050991c6439c489e281a96af87537ab941d2f24
Mihai Babet <mihai.babet@gmail.com>
2025-03-14 11:32:03 +0200

    create racetrack

--- app/games/roulette/RouletteGame/index.jsx
+++ app/games/roulette/RouletteGame/index.jsx
@@ -3,77 +3,141 @@ import "./index.scss"
 import _ from "lodash"
 
 
-const SIZE = 3
+const SIZE = 10
 
 const RouletteGame = React.memo(() => {
   return (
     <div className="RouletteGame_root">
-      <svg className="RouletteGame_table" viewBox="0 0 100 100">
-        <RouletteNumber
+      <svg className="RouletteGame_table" viewBox="0 0 140 140">
+        <RouletteBet
           x={0}
           y={0}
-          width={SIZE}
-          height={SIZE * 3}
+          width={1}
+          height={3}
           color="green"
-          number={0}
+          label={0}
         />
         {_.range(12).map((row) => {
           return (
             <g key={row}>
-
               {_.range(3).map((column) => {
                 const number = (row * 3) + (3 - column)
-                const x = (row + 1) * SIZE
-                const y = column * SIZE
+                const x = (row + 1)
+                const y = column
                 const color = number % 2 === 0 ? "red" : "transparent"
                 return (
-                  <RouletteNumber
+                  <RouletteBet
                     key={column}
                     x={x}
                     y={y}
                     color={color}
-                    number={number}
+                    label={number}
                   />
                 )
               })}
             </g>
           )
         })}
-
+        {_.range(3).map((row) => {
+          const labels = ["1st", "2nd", "3rd"]
+          return (
+            <RouletteBet
+              key={row}
+              x={13}
+              y={row}
+              color="green"
+              label={labels[2 - row]}
+            />
+          )
+        })}
+        {_.range(3).map((row) => {
+          const labels = ["1st 12", "2nd 12", "3rd 12"]
+          return (
+            <RouletteBet
+              key={row}
+              x={(1 + 4 * row)}
+              y={3}
+              width={4}
+              color="green"
+              label={labels[row]}
+            />
+          )
+        })}
+        <RouletteBet
+          x={1}
+          y={4}
+          width={2}
+          color="green"
+          label={"1 to 18"}
+        />
+        <RouletteBet
+          x={11}
+          y={4}
+          width={2}
+          color="green"
+          label={"19 to 36"}
+        />
+        <RouletteBet
+          x={3}
+          y={4}
+          width={2}
+          color="green"
+          label={"EVEN"}
+        />
+        <RouletteBet
+          x={9}
+          y={4}
+          width={2}
+          color="green"
+          label={"ODD"}
+        />
+        <RouletteBet
+          x={5}
+          y={4}
+          width={2}
+          color="red"
+        />
+        <RouletteBet
+          x={7}
+          y={4}
+          width={2}
+          color="black"
+        />
       </svg>
     </div>
   )
 })
 
 
-const RouletteNumber = React.memo(({
+const RouletteBet = React.memo(({
   x,
   y,
   color,
-  number,
-  width = SIZE,
-  height = SIZE
+  label,
+  width = 1,
+  height = 1,
+  size = SIZE
 }) => {
   return (
-    <g>
+    <g className="RouletteBet_root">
       <rect
-        x={x}
-        y={y}
-        width={width}
-        height={height}
-        strokeWidth={0.1}
-        stroke="black"
+        x={x * size}
+        y={y * size}
+        width={width * size}
+        height={height * size}
+        strokeWidth={0.3}
+        stroke="white"
         fill={color}
       />
       <text
-        x={x + (width / 2)}
-        y={y + (height / 2)}
+        x={(x + (width / 2)) * size}
+        y={(y + (height / 2)) * size}
         textAnchor="middle"
         fill="white"
         dominantBaseline="middle"
-        fontSize={Math.min(width, height) * 0.4}
+        fontSize={size / 2.5}
       >
-        {number}
+        {label}
       </text>
     </g>
   )

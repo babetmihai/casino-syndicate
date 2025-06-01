19059e4632735f083a41d22420e7e575ae0667fb
Mihai Babet <mihai.babet@gmail.com>
2025-06-01 11:43:26 +0300

    cleanup

--- server/db.js
+++
@@ -1,6 +0,0 @@
-const PouchDB = require("pouchdb")
-const path = require("path")
-
-const db = new PouchDB(path.join(__dirname, "../db"))
-
-module.exports = db
--- server/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv")
-dotenv.config()
-
-const server = require("./socket")
-
-const { SERVER_PORT } = process.env
-server.listen(SERVER_PORT, () => {
-  console.log(`Server running on http://localhost:${SERVER_PORT}`)
-})
